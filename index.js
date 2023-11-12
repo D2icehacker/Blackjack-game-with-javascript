beb1e89f0e7d041c39daa29ae7237ac6422cf3f8
Hacker D2ice <D2ice00@gmail.com>
2023-11-12 11:26:57 +0100

    first commit

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
-let countEl = document.getElementById("count-el")
-let saveEl = document.getElementById("save-el")
-let count = 0
+let firstcard = 10
+let secondcard = 4
+let cards = [firstcard, secondcard]
+let sum = firstcard + secondcard 
+let hasblackjack = false
+let isalive = true
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardEl = document.getElementById("cards-el")
 
-function increment() {
-    count += 1
-    countEl.textContent = count
-} 
-
-function save() {
-    let countStr = count + " - "
-    saveEl.textContent += countStr
-    countEl.textContent = 0
-    count = 0
+function startgame(){
+    rendergame()
 }
 
+
+function rendergame() {
+
+    cardEl.textContent = "Cards: " + cards[0] + " " + cards[1]
+    sumEl.textContent = "Sum: " + sum
+
+    if (sum <= 20) {
+        message = "Do you want to draw a new card? "
+    } else if (sum === 21) {
+        message = "Wohoo! You've got Blackjack!  "
+        hasblackjack = true
+    } else {
+        message = "You're out of the game!  "
+        isalive = false
+    }
+    messageEl.textContent = message
+
+}
+
+function newcard() {
+     let card = 6
+     sum += card
+     cards.push(card)
+     rendergame()
+}
